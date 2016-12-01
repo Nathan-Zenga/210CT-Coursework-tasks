249e8d212a1b31f77a28324a3f18aba34bdd544a
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2016-12-01 00:41:33 +0000

    Added files as of 01/12/16

--- cw q10 - sub-sequence in ascending order.py
+++ cw q10 - sub-sequence in ascending order.py
@@ -1,23 +1,29 @@
-n_list = [71, 41, 15, 68, 49, 9, 26, 46, 15, 53, 96]
+array = [71, 41, 15, 68, 49, 9, 26, 46, 15, 53, 96]
+array2 = [71, 41, 15, 68, 49, 9, 26, 46, 15, 53, 96]
 
-print(n_list)
+print(array, end="\n\n")
 
-##def subSeq(list1):
-##    list2 = []
-##    lowest = list1[0]
-##    for x in range(len(list1)-1):
-##        if list1[x+1] > list1[x]:
-##            list2.append(list1[x+1])
-##    return list2
-
-def subSeq(list1):
-    list2 = []
+def subSeq(list1, list2 = []):
+    
     lowest = list1[0]
-    while list1 < :
-        for x in range(len(list1)-1):
-            if list1[x] < list1[x+1]:
-                lowest = list1[x]
-        list2.append(lowest)
+
+    if len(list1) == 2:
+        list2.append(list1[-1])                 ## last remaining value added to list2 as it is largest by default
+
+    elif lowest > list1[1]:
+##        lowest = list1[1]                       ## next index value assigned as lowest vlaue if temp lowest value is bigger than value in the next index
+        return subSeq(list1[1:])                ## recall function to read list starting from value in second index (now the temp lowest value)
+
+    elif lowest < list1[1]:
+        list2.append(lowest)                    ## append current temp lowest value if next index value smaller than
+
+        if len(list1) == 2:
+            list2.append(list1[1])
+        else:
+            return subSeq(list1, list2)
+
+##        return subSeq(list1[lowest:])         ## recall function to read list starting from lowest value
+
     return list2
 
-print(subSeq(n_list))
+print(subSeq(array))
