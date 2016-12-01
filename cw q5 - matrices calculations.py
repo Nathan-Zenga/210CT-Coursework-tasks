f93c00332643b3874523608b400a0396d6c4991c
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2016-12-01 17:28:04 +0000

    Rearranged one of the if-statements - 1/12/16

--- cw q5 - matrices calculations.py
+++ cw q5 - matrices calculations.py
@@ -1,22 +1,22 @@
-m1 = [[87, 14, 77],
-      [49, 24, 59],
-      [23, 8, 99]]
+m1 = [[40, 17, 82],
+      [13, 30, 93],
+      [2, 33, 6]]
 
 m2 = [[35, 89, 22],
       [16, 41, 4],
       [1, 55, 61]]
 
 def addM(matrix1, matrix2):
-    if len(matrix1) != len(matrix2):
-        return None
-    else:
-        newMatrix = [[0 for i in range(len(matrix1[x]))] for x in range(len(matrix1))]
-        # adding empty to new array equal to size length of one of given lists
+    newMatrix = [[0 for i in range(len(matrix1[x]))] for x in range(len(matrix1))]
+    # adding spaces to new array, equal to size length of one of given matrices
         
-        for i in range(len(matrix1)):
+    for i in range(len(matrix1)):
+        if len(matrix1) != len(matrix2) or len(matrix1[i]) != len(matrix2[i]):
+            return None
+        else:
             for j in range(len(matrix1[i])):
                 newMatrix[i][j] = matrix1[i][j] + matrix2[i][j] # adds each value in given matrices by index
                 sumElement = newMatrix[i][j]                    
-        return newMatrix
+    return newMatrix
 
-print("%s +\n%s \n= \n%s" % ( m1, m2, addM(m1, m2) ))
+print(addM(m1, m2))
