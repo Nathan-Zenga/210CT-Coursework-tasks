6151dddb4b5147f5f39cc33a506e0d04633f2507
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2016-12-02 17:52:41 +0000

    File change - 2/12/16 (2)

--- cw2 advanced q1 b - union, written once.py
+++ cw2 advanced q1 b - union, written once.py
@@ -2,6 +2,7 @@ string1 = ['p', 'w', 'q', 'r', 'w', 'a', 'y', 'g', 'z', 'v']
 string2 = ['e', 'w', 'u', 't', 'x', 'o', 'u', 'a', 'p', 'a']
 
 def unionString(s1, s2):
+    '''Returns new string containing union of given strings, removing any duplicates'''
     s3 = string1 + string2  # concatenates both lists into one
     s3 = set(s3)            # converts concatenaton to set so each element is written once
     return s3
