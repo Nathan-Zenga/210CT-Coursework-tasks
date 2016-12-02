72d62b24a737ef61b9baa1ce1cfd80ac982029ac
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2016-12-02 17:50:53 +0000

    File changes - 2/12/16

--- cw2 advanced q1 a - intercept.py
+++ cw2 advanced q1 a - intercept.py
@@ -2,14 +2,12 @@ list1 = ['p', 'w', 'q', 'r', 'w', 'a', 'y', 'g', 'z', 'v']
 list2 = ['e', 'w', 'u', 't', 'x', 'o', 'u', 'a', 'p', 'a']
 
 def intercept(l1, l2):
+    '''Returns a new list containing all elements present in all given strings'''
     list3 = []
     for i in range(len(l1)):
         for j in range(len(l2)):
-            if l1[i] == l2[j]:
-                list3.append(l1[i])
-##            while i <= len(list3)-1 and j <= len(list3)-1:
-##                if list3[i] == list3[j]:
-##                    list3.pop(i)
+            if l1[i] == l2[j]:      # checks if element in first string is also found in second string
+                list3.append(l1[i]) # appends to list3
     return list3
 
 print(intercept(list1, list2))
--- cw2 advanced q1 b - union, written once.py
+++ cw2 advanced q1 b - union, written once.py
@@ -1,35 +1,9 @@
 string1 = ['p', 'w', 'q', 'r', 'w', 'a', 'y', 'g', 'z', 'v']
 string2 = ['e', 'w', 'u', 't', 'x', 'o', 'u', 'a', 'p', 'a']
 
-s3 = string1 + string2
-
-
-print(s3)
-
 def unionString(s1, s2):
-    count = 0
-    for x in range(len(s3)):
-        for y in range(len(s3)):
-            if s3[x] == s3[y]:
-                count = 1
-                
-            if count == 1:
-                s3.pop(x)
-                count=0
-    
+    s3 = string1 + string2  # concatenates both lists into one
+    s3 = set(s3)            # converts concatenaton to set so each element is written once
     return s3
-
-##def unionString(s1, s2):
-##    count = 0
-##    for x in range(len(s1)):
-##        for y in range(len(s2)):
-##            if s1[x] == s2[y]:
-##                count += 1
-##                if count  1:
-##                    s2.pop(x)
-##                    count = 0
-##    string3 = s1 + s2
-##    return string3
-
 
 print(unionString(string1, string2))
