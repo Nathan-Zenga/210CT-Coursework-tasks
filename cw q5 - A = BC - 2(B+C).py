5bbded82283aa434731cc81716b68f6fe507a49c
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2016-12-02 23:31:08 +0000

    Add files via upload

--- cw q5 - A = BC - 2(B+C).py
+++ cw q5 - A = BC - 2(B+C).py
@@ -1,55 +1,18 @@
-m1 = [[40, 17, 82],
-      [13, 30, 93],
-      [2, 33, 6]]
+B = [[1,2],[4,8]]
+C = [[5,7],[6,1]]
 
-m2 = [[35, 89, 22],
-      [16, 41, 4],
-      [1, 55, 61]]
+sub = __import__('cw q5 - matrix subtraction')
+add = __import__('cw q5 - matrix addition')
+multi = __import__('cw q5 - matrix multiplication')
 
-def addM(matrix1, matrix2):
-    '''Returns a new matrix with each value equal to the sum of each index values of the given matrices'''
-    newMatrix = [[0 for i in range(len(matrix1[x]))] for x in range(len(matrix1))]
-    # 0s added to each index of inner list; inner list added to each index of main list
-    # indicates empty spaces to new array, equal to size length of one of given matrices
-        
-    for i in range(len(matrix1)):
-        if len(matrix1) != len(matrix2) or len(matrix1[i]) != len(matrix2[i]):
-            return None
-        else:
-            for j in range(len(matrix1[i])):
-                newMatrix[i][j] = matrix1[i][j] + matrix2[i][j] # gives sum of vlaues of both given matrices by index
-    return newMatrix
+r1 = add.addM(B, C)
+r2 = multi.multiM(B, C)
+r3 = multi.multiM(r1, 2)
+## lists returned from above functions are stored
 
-def subM(matrix1, matrix2):
-    '''Returns a new matrix with each value equal to the subtraction of each index values of the first matrix by that of the second matrix'''
-    newMatrix = [[0 for i in range(len(matrix1[x]))] for x in range(len(matrix1))]
-        
-    for i in range(len(matrix1)):
-        if len(matrix1) != len(matrix2) or len(matrix1[i]) != len(matrix2[i]):
-            return None
-        else:
-            for j in range(len(matrix1[i])):
-                newMatrix[i][j] = matrix1[i][j] - matrix2[i][j] # subtracts each value in given matrices
-    return newMatrix
+A = sub.subM(r2, r3) ## computes A = B*C - 2*(B+C)
 
-def multiM(matrix1, matrix2):
-    '''Returns a new matrix with each value equal to the multiplication of given matrices'''
-    newMatrix = [[0 for i in range(len(matrix1))] for x in range(len(matrix1[0]))]
-    # determines that the dimension product of new matrix will be height (i) of matrix1 and width (j) of matrix2
-        
-    for i in range(len(matrix1)-1):
-        for j in range(len(matrix1[i])-1):
-            newMatrix[i][j] = (matrix1[i][j] * matrix2[i][j]) + (matrix1[i][j+1] * matrix2[i+1][j]) # multiplies values in given matrices by index
-    return newMatrix
-
-print(addM(m1, m2))
-print(subM(m1, m2))
-
-m1 = [[7, 8],
-      [0, 9],
-      [3, 6]]
-
-m2 = [[5, 9, 2],
-      [6, 1, 4]]
-
-print(multiM(m1, m2))
+print(r1)
+print(r2)
+print(r3)
+print(A)
